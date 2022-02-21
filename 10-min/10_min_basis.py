07ddad4655799f47fa9e3c90bb0d64b8105462fd
windpowerstake <windpowerstake@protonmail.com>
2022-02-21 15:23:18 +0100

    adding 10min-v2

--- 10-min/10_min_basis.py
+++ 10-min/10_min_basis.py
@@ -2,26 +2,31 @@ import os
 import datetime
 import time
 
+#folder is created if it does not exist
+dir_logs= "~/logEnergy/history/"
+if not os.path.exists(dir_logs):
+	os.makedirs(dir_logs)
+
 
 #infinite loop repeated every 10 minutes, quick and dirty - time series buffered snaps are also possible
 while True:
 
 	#round to the next available 10 minute interval
-	tm=datetime.datetime.utcnow()
-	discard = datetime.timedelta(minutes=tm.minute%10,seconds=tm.second,microseconds=tm.microsecond)
-	tm -= discard
+	time_moment=datetime.datetime.utcnow()
+	discard = datetime.timedelta(minutes=time_moment.minute%10,seconds=time_moment.second,microseconds=time_moment.microsecond)
+	time_moment -= discard
 	if discard >= datetime.timedelta(minutes=5):
-		tm += datetime.timedelta(minutes=10)
+		time_moment += datetime.timedelta(minutes=10)
 
 	#result after catching delta and rounding
-	vTimestamp=str(tm.strftime('%Y-%m-%d %H:%M:%S'))+"Z"
+	variable_timestamp=str(time_moment.strftime('%Y-%m-%d %H:%M:%S'))+"Z"
 
 
 
 	#print and save to history
-	newSCADAln=vTimestamp+","+"var1"+","+"var2"+","+"var3"
+	newSCADAln=variable_timestamp+","+"var_1"+","+"var_2"+","+"var_3"
 	print(newSCADAln)
-	with open("./logEnergy/history/hist_log.txt", "a") as file_object:
+	with open(dir_logs+"hist_log.txt", "a") as file_object:
 		file_object.write(newSCADAln+"\n")
 
 	time.sleep(60*10)
