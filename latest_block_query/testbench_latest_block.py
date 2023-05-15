6df93e0848fbdee564bea38f9616b94744cfa8c5
windpowerstake <93733899+windpowerstake@users.noreply.github.com>
2023-05-16 00:27:18 +0200

    Update testbench_latest_block.py

--- latest_block_query/testbench_latest_block.py
+++ latest_block_query/testbench_latest_block.py
@@ -4,11 +4,17 @@ import time
 import string
 from subprocess import PIPE, Popen
 
+#in order for this testbench to run you need to have compiled the comparer file
+#g++ log_search.cpp -o log_search
+#please resolve any dependencies, e.g.: sudo apt install build-essential
+#this program and log_search need to be in the .blockchain folder in order to take it as a reference, but this can be easily tweaked
 blockchain="cerberusd"
 #yes, this is running on a cerberus isolated instance
 #in theory chain does not be to running in order to run an algorithm like this, but I have not tested it for a stopped chain
 
-#status popen
+#the plan is to get it into production on other nodes later on
+
+#status of the blockchain, we'll use popen
 print("same status but with Popen")
 a = datetime.datetime.now()
 command = blockchain + ' status 2>&1 | jq "{latest_block_height: .SyncInfo.latest_block_height}"'
@@ -26,7 +32,7 @@ c1 = b - a
 print("status query: "+str(c1))
 print("")
 
-print("give me all files containing the previous string...")
+print("now execute the log_search, which searches the last .log file in blockstore.db and then searches for the last block registered there...")
 a = datetime.datetime.now()
 
 
